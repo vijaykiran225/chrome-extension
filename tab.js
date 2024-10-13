182db4849f0f4bd347be99055f8be79a29e3c3c5
Vijay Kiran <vijaykiran225@gmail.com>
2024-10-13 22:33:07 +0530

    search history and bookmarks

--- tab.js
+++ tab.js
@@ -34,13 +34,14 @@ function showDate() {
 		"Oct",
 		"Nov",
 		"Dec"];
-	const DAYS = ["Mon",
+	const DAYS = [
+		"Sun",
+		"Mon",
 		"Tue",
 		"Wed",
 		"Thu",
 		"Fri",
-		"Sat",
-		"Sun"];
+		"Sat"];
 
 
 	let content = ` ${DAYS[d.getDay()]}  ${d.getDate()} / ${MON[d.getMonth()]} / ${d.getFullYear()} `;
@@ -52,13 +53,109 @@ function showTime() {
 	// body...
 	let d = new Date();
 
-	let content = `Its about ${d.getHours()} : ${d.getMinutes()}`;
+	let content = `Its ${d.getHours()} : ${d.getMinutes()}`;
 
 	return content;
+}
+
+function mockMode(targetName) {
+	showHistoryInPage(myhist.filter(x => (x.title.toLowerCase().indexOf(targetName.toLowerCase()) != -1)), "history_search_result");
+}
+
+function search(targetName) {
+	searchHistory(targetName)
+	searchBookmark(targetName)
+}
+
+function fillData(event) {
+	if (event.keyCode == 13 || event.target.id == "fillData") {
+		//enter key was pressed
+		console.log(event)
+		let targetName = document.getElementById("ip").value;
+		// mockMode(targetName)
+		search(targetName)
+	}
+
+}
+
+function searchHistory(key) {
+
+	chrome.history.search({
+		text: key,
+		startTime: 1640995200000,
+		maxResults: 10000
+	}, (xList) => {
+		showHistoryInPage(xList, "history_search_result");
+
+	})
+
+}
+
+function searchBookmark(key) {
+	chrome.bookmarks.search(
+		{ query: key },
+		(resp) => {
+			showHistoryInPage(resp, "bookmarks_search_result");
+		}
+	)
 }
 
 document.getElementById('welcome_msg').innerText = greetVijay();
 document.getElementById('time_msg').innerHTML = showTime();
 document.getElementById('date_msg').innerHTML = showDate();
+document.getElementById("fillData").addEventListener("click", fillData);
+document.getElementById("ip").addEventListener("keydown", fillData);
 
-// document.getElementsByTagName("body
+function breakTag() {
+	return document.createElement('br');
+}
+
+function anchorTag(url, text) {
+	let linkA = document.createElement('a');
+	linkA.href = url;
+	linkA.text = text;
+	return linkA;
+}
+
+function highlight(text) {
+	let innerHTML = text;
+	let targetName = document.getElementById("ip").value;
+	var index = innerHTML.toLowerCase().indexOf(targetName.toLowerCase());
+	if (index >= 0) {
+		innerHTML = innerHTML.substring(0, index) + "<span style='background-color: yellow;border:solid 1px red;font-size:12pt'>" + innerHTML.substring(index, index + targetName.length) + "</span>" + innerHTML.substring(index + targetName.length);
+	}
+	return innerHTML;
+}
+function h2Tag(text) {
+	let h2Ele = document.createElement('div');
+	h2Ele.innerHTML = highlight(text);
+	return h2Ele;
+}
+
+function makeCustomDiv(data) {
+	let myDiv = document.createElement('div');
+	myDiv.style.border = 'solid 1px black';
+	myDiv.style.padding = '15px';
+	myDiv.style.margin = '5px';
+
+	myDiv.appendChild(h2Tag(data.title));
+	myDiv.appendChild(h2Tag(data.url));
+
+	myDiv.appendChild(breakTag());
+
+	myDiv.appendChild(anchorTag(data.url, data.title));
+
+	return myDiv;
+}
+function showHistoryInPage(xList, tagName) {
+	document.getElementById(tagName).innerHTML = "";
+	document.getElementById(tagName + '_count').innerHTML = xList.length;
+
+	for (let x of xList) {
+
+		document.getElementById(tagName).append(breakTag());
+		document.getElementById(tagName).append(makeCustomDiv(x));
+
+	}
+}
+
